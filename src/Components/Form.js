49f6021a6782f2731bb7b636bc174c54ed7d9438
StanTheMan7 <sts.graur@gmail.com>
2022-06-24 10:10:08 +0200

    Afficer a liste, supprimer item, ajouter todo

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,33 +1,66 @@
 import {useState} from 'react'
 import Item from './Item'
+// uuid import pour avoir des cle unique
+import {v4 as uuidv4} from 'uuid'
 
 
 export default function Form(){
 
     const [dataArr, setDataArr] = useState([
-        {txt : 'Promener le chien'},
-        {txt : 'Sport'},
-        {txt : 'Coder en React'},
+        // chaque element va avoir un id de base
+        {txt : 'Promener le chien', id: uuidv4()},
+        {txt : 'Sport', id: uuidv4()},
+        {txt : 'Coder en React', id: uuidv4()},
     ])
+    // creer la function delete qui prend en parametre 'id'
+    const deleteElement = id => {
+        // pour filtrer les 'li' on va utiliser la methode  filter()
+        const filteredState = dataArr.filter(item => {
 
+            return item.id !== id
+        })
+        setDataArr(filteredState)
+    }
+
+    const [stateInput, setStateInput] = useState();
+    
+    const linkedInput = (e) => {
+        setStateInput(e)
+    }
+
+    const addTodo = e => {
+        // ca va prevenir le comportement par defaut d'acctualiser la page , ca va pas actualiser la page 
+        e.preventDefault();
+        const newArr = [...dataArr]; // spread operator va copier tout ce que on a dans le tableau dataArr dans newArr
+
+        const newTodo = {};
+        newTodo.txt = stateInput;
+        newTodo.id = uuidv4();
+        newArr.push(newTodo);
+        setDataArr(newArr);
+        setStateInput('');
+    }
     return(
         <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
 
-            <form className='mb-3'>
+            <form onSubmit={e => addTodo(e)} className='mb-3'>
                 <label htmlFor="todo" className='form-label mt-3'>Chose à faire</label>
-                <input type="text" className='form-control' id='todo' />
+                <input onInput={e => linkedInput(e.target.value)} value={stateInput} type="text" className='form-control' id='todo' />
                 <button className="mt-2 btn-primary d-block">Envoyer</button>
             </form>
 
             <h2>liste de choses à faire :</h2>
 
             <ul className="list-group">
-                {dataArr.map((item, index) => {
+                {dataArr.map(item => {
                     return (
                         // props
                         <Item 
                         txt={item.txt} // le text qui vien de notre state
-                        key={index}  // la cle pour chaque element 
+                        key={item.id}  // l'cle unique depui le state pour react
+                        id={item.id} // l'id
+                        // comme on veut utiliser la function deleteElement il faut la passer en props 
+                        delFunc={deleteElement}
                         />
                     )
                 })}
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -4,7 +4,8 @@ export default function Item(props) {
   return (
     <li className="border d-flex justify-content-between align-items-center  p-2 m2">
         <div className="p-3">{props.txt}</div>
-        <button className="btn btn-danger p-2 h-5">Supprimer</button>
+        {/* // qund on apuis sur le button on appele props.delFunc et on va lui passe props.id et on fais ca avec une function anonyme pour eviter que la function delFunc() se execute  */}
+        <button className="btn btn-danger p-2 h-5" onClick={() => props.delFunc(props.id)}>Supprimer</button>
     </li>
   )
 }
